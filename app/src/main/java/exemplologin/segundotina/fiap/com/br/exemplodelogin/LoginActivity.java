b8d2b6b101c7fa05e9ac74c33f2a36c3092da68c
Julio Cesar <juliocsvilela@outlook.com>
2017-02-15 11:44:37 -0200

    Finalizado exemplo login

--- app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplodelogin/LoginActivity.java
+++ app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplodelogin/LoginActivity.java
@@ -1,13 +1,45 @@
 package exemplologin.segundotina.fiap.com.br.exemplodelogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void logar(View v) {
+
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+
+
+        EditText edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
+        String nome = edtUsuario.getText().toString();
+        String senha = edtSenha.getText().toString();
+
+        if (nome.toLowerCase().equals("FIAP")|| senha.toLowerCase().equals("FIAP123")){
+
+            Toast.makeText(this, "Seu nome " + nome + "Senha" + senha , Toast.LENGTH_SHORT).show();
+
+        }else {
+            Toast.makeText(this, getString(R.string.mensagem_erro), Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
+
+
+
 }
---
+++ app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplodelogin/MainActivity.java
@@ -0,0 +1,20 @@
+package exemplologin.segundotina.fiap.com.br.exemplodelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
+
+
